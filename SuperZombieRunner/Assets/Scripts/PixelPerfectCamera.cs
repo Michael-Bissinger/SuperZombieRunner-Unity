2c406b1f817bb5d8ece7dd537c94513919d34d96
Michael Bissinger <Michael.Bissinger@inb.de>
2019-04-16 12:48:49 +0200

    create quad and add a texture to it (wall)

--- SuperZombieRunner/Assets/Scripts/PixelPerfectCamera.cs
+++ SuperZombieRunner/Assets/Scripts/PixelPerfectCamera.cs
@@ -7,7 +7,7 @@ public class PixelPerfectCamera : MonoBehaviour
 
 	public static float pixelsToUnits = 1f;
 	public static float scale = 1f;
-
+    
 	public Vector2 nativeResolution = new Vector2 (240, 160);
 
 	void Awake() {
@@ -16,7 +16,7 @@ public class PixelPerfectCamera : MonoBehaviour
 		if(camera.orthographic) {
 			scale = Screen.height/nativeResolution.y;
 			pixelsToUnits *= scale;
-			camera.orthographicSize = (Screen.height / 2.0f) / pixelsToUnits);
+			camera.orthographicSize = (Screen.height / 2.0f) / pixelsToUnits;
 		}
 
 
