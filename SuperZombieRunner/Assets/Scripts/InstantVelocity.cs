e0b3316abf41a10ba9a2c166c097699ceca31896
Michael Bissinger <Michael.Bissinger@inb.de>
2019-04-18 16:26:04 +0200

    write script InstantVelocity to move the obstacle

---
+++ SuperZombieRunner/Assets/Scripts/InstantVelocity.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantVelocity : MonoBehaviour
+{
+
+    public Vector2 velocity = Vector2.zero;
+
+    private Rigidbody2D body2d;
+
+    void Awake()
+    {
+        body2d = GetComponent<Rigidbody2D> ();
+    }
+
+    private void FixedUpdate()
+    {
+        body2d.velocity = velocity;
+
+    }
+
+}
