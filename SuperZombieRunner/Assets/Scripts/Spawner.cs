f88e4c6d109966452d0dd79f19e3dd76051fab6a
Michael Bissinger <Michael.Bissinger@inb.de>
2019-07-29 14:11:31 +0200

    create constants for spawner-script

--- SuperZombieRunner/Assets/Scripts/Spawner.cs
+++ SuperZombieRunner/Assets/Scripts/Spawner.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+
+    public GameObject[] prefabs; // contains all prefabs, that the spawner can make
+    public float delay = 2.0f;
+    public bool active = true;
+
+
     // Start is called before the first frame update
     void Start()
     {
